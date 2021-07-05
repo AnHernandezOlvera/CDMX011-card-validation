038ab0f74169c9c9d75b6919963682ef11146b4f
An <anahidisenadora@gmail.com>
2021-07-05 09:26:02 -0500

    feat/isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import validator from './validator.js';
 
+document.getElementById('validate').addEventListener('click', function () {
+    const cardNumber = document.getElementById('value').value;
+    console.log(cardNumber)
+    validator.isValid(cardNumber);
+})
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+  isValid: function(cardNumber) {
+    const numbers = Array.from(cardNumber)
+    let sum = 0;
+
+    // Step 1: doubling digits in odd positions
+    for(let i = 0; i < numbers.length; i++) {
+      if (i % 2 === 0) {
+        numbers[i] = Number(numbers[i]) * 2
+      } else {
+        numbers[i] = Number(numbers[i])
+      }
+    }
+
+
+    // Step 2: Sum digits of numbers greater than 9
+    for(let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > 9) {
+        numbers[i] = 1 + numbers[i] % 10
+      }
+    }
+
+    // Step 4: sum all digits
+    for(let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+
+    if(sum % 10 === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
 };
 
 export default validator;
